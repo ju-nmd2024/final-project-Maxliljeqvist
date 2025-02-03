5c4acbf453976a39b9dac0dd455a9c816390debe
Maxliljeqvist <lima24nb@student.ju.se>
2025-02-03 23:45:27 +0100

    Class
    added a class for the player (doodler), and made it bounce.

--- game.js
+++ game.js
@@ -1,15 +1,55 @@
 
 let x;
 let y = 0;
-let touchingPlatform = true;
+let touchingPlatform = "";
 let state = "game";
 let moving = true;
-let bollState = true
+let bollObj;
+
+class doodler {
+  constructor(x, y) {
+      this.x = x;
+      this.y = y;
+      this.speed = 0;   // Startar stillastående
+      this.gravity = 0.4;  // Gravitationseffekt
+      this.bounceStrength = -10; // Konstant studs-hastighet
+  }
+
+  draw() {
+      fill(255, 0, 0);
+      ellipse(this.x, this.y, 70, 70);
+  }
+
+  update() {
+      this.speed += this.gravity; // Lägg till gravitation till hastigheten
+      this.y += this.speed;       // Uppdatera bollens position
+
+      // Om bollen träffar marken
+      if (this.y >= 600) { 
+          this.y = 600; // Håll den ovanför marken
+          this.speed = this.bounceStrength; // Studsa upp med konstant kraft
+          
+      }
+      if (moving) {
+        if (keyIsDown(LEFT_ARROW)) {
+          this.x -= 10;
+        }
+    
+        if (keyIsDown(RIGHT_ARROW)) {
+          this.x += 10;
+        }
+    
+      }
+  }
+}
 
 function setup() {
   createCanvas(500,650);
   x = 200;
   y = 550;
+
+  bollObj = new doodler(x, y);
+
 }
 
 function startScreen() {
@@ -24,11 +64,11 @@ function gameScreen() {
  
   if (moving) {
     if (keyIsDown(LEFT_ARROW)) {
-      x -= 5;
+      x -= 10;
     }
 
     if (keyIsDown(RIGHT_ARROW)) {
-      x += 5;
+      x += 10;
     }
 
   }
@@ -48,18 +88,16 @@ touchingPlatform = false;{
 }
   }
 
-  doodler(x,y);
-
-}
-
-
-function doodler(x,y,s) {
-  fill(0);  
-  rect(x,y,50,50);
   
+
 }
+
+
 
 
 function draw() {
    clear();
-  gameScreen();}
+  gameScreen();
+  bollObj.update();
+  bollObj.draw();
+}
