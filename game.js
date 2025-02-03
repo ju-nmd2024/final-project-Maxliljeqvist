cf3b2721192d896393203fbfe3d1a216507d9bd1
Maxliljeqvist <lima24nb@student.ju.se>
2025-02-03 23:27:52 +0100

    movement
    Added a rect and made it movable

--- game.js
+++ game.js
@@ -1,13 +1,15 @@
 
 let x;
 let y = 0;
+let touchingPlatform = true;
 let state = "game";
-
-
+let moving = true;
+let bollState = true
 
 function setup() {
-  createCanvas(800,600);
-  
+  createCanvas(500,650);
+  x = 200;
+  y = 550;
 }
 
 function startScreen() {
@@ -18,20 +20,46 @@ function startScreen() {
 }
 
 function gameScreen() {
-  background(100);
-  fill(255);
-  textSize(24);
-  text("Game", 300, 300);
+  background(255);
+ 
+  if (moving) {
+    if (keyIsDown(LEFT_ARROW)) {
+      x -= 5;
+    }
 
+    if (keyIsDown(RIGHT_ARROW)) {
+      x += 5;
+    }
+
+  }
+
+  if (touchingPlatform){
+    touchingPlatform = true;{
+      y -= 250;
+      if (y < 200){
+        y = 200;
+      }
+    }
+touchingPlatform = false;{
+  y += 250;
+  if (y > 650){
+    y = 650;
+  }
+}
+  }
+
+  doodler(x,y);
 
 }
 
 
 function doodler(x,y,s) {
-  fill(255);
-  ellipse(x,y,s);
-  ellipse(x-10,y-10,s/2);
-  ellipse(x+10,y-10,s/2);
-  ellipse(x-15,y+10,s/2);
-  ellipse(x+15,y+10,s/2);
+  fill(0);  
+  rect(x,y,50,50);
+  
 }
+
+
+function draw() {
+   clear();
+  gameScreen();}
