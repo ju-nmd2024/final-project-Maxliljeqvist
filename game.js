1e657ade904f21cc43a00dccb1d79e46750ba174
Maxliljeqvist <lima24nb@student.ju.se>
2025-02-05 09:44:18 +0100

    adding a game over screen
    Fixing the game states so that you can lose.

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ let bollObj;
 let gap;
 let score = 0;
 let platforms = [];
-
+let state = "gameScreen";
 
 function setup() {
   createCanvas(500, 650);
@@ -29,6 +29,28 @@ function startScreen() {
 
 function gameScreen() {
   background(255);
+
+  if (bollObj.y > 650) {
+    state = "gameOver";
+}
+}
+
+function gameOver() {
+  background(0);
+  fill(255);
+  textSize(24);
+  text("Game Over", 300, 300);
+  text("Score: " + Math.floor(score), 300, 350);
+  text("Press Enter to Restart", 300, 400);
+  bollObj.y = 600;
+  bollObj.velocity = 0;
+  score = 0;
+  platforms = [];
+  let numbPlatforms = 6;
+  gap = height / numbPlatforms;
+  for (let i = 1; i < 6; i++) {
+    platforms.push(new Platform(random(width), height - i*gap));
+  }
 }
 
 function draw() {
